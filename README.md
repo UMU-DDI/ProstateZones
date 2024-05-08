4548b139b9a314f41eebbd54aec494b3f86b7e44
Attila Simkó <attila@nonpimedical.com>
2024-05-08 13:11:39 +0200

    Updated files, readme

--- README.md
+++ README.md
@@ -1,1 +1,109 @@
 # ProstateZones
+
+This repository contains files to organize the data and ease the use of the ProstateZones dataset.
+The ProstateZones dataset is a collection of segmentations of the prostatic zones and urethra delineated on publicly available images from the PROSTATEx dataset. The dataset is intended for research purposes in the field of medical imaging, particularly focusing on automatic segmentations of the prostate, prostatic zones, and urethra.
+
+
+## Usage
+The files in this repository are structured to facilitate organization and utilization of the ProstateZones dataset.
+
+### Download
+To start, download the images and segmentations.
+
+#### Images
+The images can be downloaded from the Cancer Imaging Archive (TCIA) at the following link: https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=23691656
+#### Segmentations
+The segmentations are available on Zenodo, accessible through the DOI: 10.5281/zenodo.10718469
+
+### Structure Data
+To pair the images and segmentations, and divide into Train/Validate/Test folders, run the followig lines of code:
+
+```ruby
+pip install > requirements.txt
+```
+
+```ruby
+python data_to_folders.py --image_folder 'path/to/PROSTATEx_image_folder' --segmentation_folder 'path/to/PorstateZones_segmentation_folder' --output_folder 'path/to/output_folder' --images T2_Sag T2_Cor DWI ADC HBV
+```
+The above lines of code will extract the Axial T2 weigthed image that was used for the manual delineations from the PROSTATEx data and copy it to a new folder. If any additional sequences are of interest, they can be added through the `--image`-flag.
+Images will be renamed into a more straightforward structure, while still keeping the 4-digit patient specifier as in the original PROSTATEx naming convention:
+
+- tra : Axial T2w
+- sag : Sagittal T2w
+- cor : Coronal T2w
+- dwi : Diffusion Weighted image
+- adc : Apparent Diffusion Coefficient
+- hbv : High B-value image
+
+#### Folder Structure:
+
+After running the above lines of code the data will have the following structure in the output folder.
+
+- **Images**
+  - ProstateX-0000
+    - _adc-0000_
+    - _cor-0000_
+    - _dwi-0000_
+    - _hbv-0000_
+    - _sag-0000_
+    - tra-0000
+  - ProstateX-0007
+  - ...
+
+- **Train**
+  - ProstateX-0000
+    - _adc-0000_
+    - _hbv-0000_
+    - tra-0000
+    - Seg-0000.nrrd
+  - ProstateX-0008
+  - ...
+
+- **Validate**
+  - ProstateX-0007
+    - _adc-0007_
+    - _hbv-0007_
+    - tra-0007
+    - Seg-0007.nrrd
+  - ProstateX-0010
+  - ...
+
+- **Test**
+  - ProstateX-0015
+    - _adc-0015_
+    - _hbv-0015_
+    - tra-0015
+    - Seg-0015_R1.nrrd
+    - Seg-0015_R2.nrrd
+  - ProstateX-0019
+  - ...
+ 
+**Note:** _italic_ images are optional in each folder depending on the inputs to the `--image`-flag, although Sagittal and Coronal images will not be included in the Train/Validate/Test folders, which (without modification) only include images with the same orientation as the segmentations.
+
+
+### Evaluation Workflow
+The evaluation workflow used to analyze the inter-reader variability data is included in this repository.
+To run the evaluation workflow, you'll need Hero, a software tool for medical image analysis. You can sign up for a free trial at their webpage: https://www.heroimaging.com/
+
+#### Setup for Hero
+To set up the data for running the workflow in Hero, watch the provided videos on loading data and batching. These videos will guide you through the process of configuring Hero to work with the ProstateZones dataset.
+
+
+## Further Information
+
+For more information about the dataset, see the Data publication:
+
+....
+
+## Citation
+If you use this dataset in your work, please cite it as:
+
+#### ProstateZones segmentations
+
+- ...
+
+#### PROSTATEx dataset
+
+- Geert Litjens, Oscar Debats, Jelle Barentsz, Nico Karssemeijer, and Henkjan Huisman. "ProstateX Challenge data", The Cancer Imaging Archive (2017). DOI: 10.7937/K9TCIA.2017.MURS5CL
+- Litjens G, Debats O, Barentsz J, Karssemeijer N, Huisman H. "Computer-aided detection of prostate cancer in MRI", IEEE Transactions on Medical Imaging 2014;33:1083-1092. DOI: 10.1109/TMI.2014.2303821
+- Clark K, Vendt B, Smith K, Freymann J, Kirby J, Koppel P, Moore S, Phillips S, Maffitt D, Pringle M, Tarbox L, Prior F. The Cancer Imaging Archive (TCIA): Maintaining and Operating a Public Information Repository, Journal of Digital Imaging, Volume 26, Number 6, December, 2013, pp 1045-1057. DOI: 10.1007/s10278-013-9622-7
--- data_to_folders.py
+++ data_to_folders.py
@@ -4,9 +4,9 @@ import numpy as np
 import os
 import re
 import shutil
+import argparse
 
 os.environ['PYTHONHASHSEED'] = str(2023)
-
 
 def copy_image(src, dst):
 
@@ -22,27 +22,42 @@ def copy_segmentation(src, dst):
 This file will take the downloaded PROSTATEx dataset + segmentations and structure them in folders.
 '''
 
+# ['T2_Ax','T2_Sag','T2_Cor','DWI','ADC','HBV']
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--images', type=str, default=[], nargs='+', help='Additional image sequences you want to include. Options are: T2_Sag, T2_Cor, DWI, ADC, and HBV.')
+parser.add_argument('--image_folder', type=str, default='C:\William\Doktorand\Data\ProstateX - Images\PROSTATEx', help='Path to the folder containing the images.')
+parser.add_argument('--segmentation_folder', type=str, default='C:\William\Doktorand\Data\ProstateX - Segmentations', help='Path to the folder containing the segmentations.')
+parser.add_argument('--output_folder', type=str, default='output', help='Path to the desired output folder.')
+args = parser.parse_args()
+
+# List of image sequences that should be included
+image_sequences = args.images
+image_sequences.insert(0, 'T2_Ax')
 
 # Path to the fodler containing the downloaded PROSTATEx dataset.
-path_to_ProstateX_data = r'...\PROSTATEx'
+path_to_ProstateX_data = args.image_folder
 
 # Path to the folder containing the downloaded segmentations.
-path_to_segmentations = r'...\ProstateSegmentations'
+path_to_segmentations = args.segmentation_folder
 
 # Path to the folder where we want to save the structured data.
-output_path = r'__Desired_Output_Directory__'
+output_path = args.output_folder
 
+# Access support txt-files
+base = os.getcwd()
+support_files = os.path.join(base, 'Support Files')
 
 
 # Create list with samples and duplicates.
 samples = []
 duplicates = []
 
-with open('_samples_.txt', "r") as lst_samples:
+with open(os.path.join(support_files, '_samples_.txt'), "r") as lst_samples:
     for s in lst_samples:
         samples.append('ProstateX-' + s.replace("\n", ""))
 
-with open('_duplicates_.txt', "r") as lst_duplicates:
+with open(os.path.join(support_files, '_duplicates_.txt'), "r") as lst_duplicates:
     for d in lst_duplicates:
         duplicates.append('ProstateX-' + d.replace("\n", ""))
 
@@ -51,21 +66,23 @@ with open('_duplicates_.txt', "r") as lst_duplicates:
 # Defining the DataFrame with the names of relevant images we will use.
 df = pd.DataFrame(columns = ['PatientID', 'T2_Ax', 'T2_Sag', 'T2_Cor', 'DWI', 'ADC', 'HBV'])
 
-with open('list_of_PROSTATEx_files.txt', 'r') as _file_:
+with open(os.path.join(support_files,'list_of_PROSTATEx_files.txt'), 'r') as _file_:
     for line in _file_:
         data = line.strip().split(':')
 
         df.loc[len(df)] = data
 
+image_sequences.insert(0, 'PatientID')
+
+# Extracting our selected samples from the dataset.
+df_sequences = df.filter(items=image_sequences)
+df_ProstateX = df_sequences[df_sequences['PatientID'].isin(samples)]
+
 ############################################
 # COPY TO OUTPUT FOLDER
 ############################################
 
-# Extracting our selected samples from the dataset.
-df_ProstateX = df[df['PatientID'].isin(samples)]
-
-
-# Copy our selected files to our output folder.
+mapping = {'T2_Ax': 'tra-', 'T2_Sag': 'sag-', 'T2_Cor': 'cor-', 'DWI': 'dwi-', 'ADC': 'adc-', 'HBV': 'hbv-'}
 
 for patient_folder in list(df_ProstateX['PatientID']):
     
@@ -75,49 +92,12 @@ for patient_folder in list(df_ProstateX['PatientID']):
 
 
     for path_Folder, folders, files in os.walk(os.path.join(path_to_ProstateX_data, patient_folder)):
-            
-            
-        if re.search(row['T2_Ax'][0], path_Folder) and row['T2_Ax'][0] != '':
 
-            copy_image(path_Folder,
-                       os.path.join(output_path, 'Images', patient_folder, 'tra-' + numbers))
-            
-            continue
+        for sequence in image_sequences[1:]:
 
-        if re.search(row['T2_Sag'][0], path_Folder) and row['T2_Sag'][0] != '':
+            if re.search(row[sequence][0], path_Folder) and row[sequence][0] != '':
 
-            copy_image(path_Folder,
-                       os.path.join(output_path, 'Images', patient_folder, 'sag-' + numbers))
-
-            continue
-        
-        if re.search(row['T2_Cor'][0], path_Folder) and row['T2_Cor'][0] != '':
-
-            copy_image(path_Folder,
-                       os.path.join(output_path, 'Images', patient_folder, 'cor-' + numbers))
-
-            continue
-
-        if re.search(row['DWI'][0], path_Folder) and row['DWI'][0] != '':
-
-            copy_image(path_Folder,
-                       os.path.join(output_path, 'Images', patient_folder, 'dwi-' + numbers))
-
-            continue
-
-        if re.search(row['ADC'][0], path_Folder) and row['ADC'][0] != '':
-
-            copy_image(path_Folder,
-                       os.path.join(output_path, 'Images', patient_folder, 'adc-' + numbers))
-
-            continue
-
-        if re.search(row['HBV'][0], path_Folder) and row['HBV'][0] != '':
-
-            copy_image(path_Folder,
-                       os.path.join(output_path, 'Images', patient_folder, 'hbv-' + numbers))
-
-            continue
+                copy_image(path_Folder, os.path.join(output_path, 'Images', patient_folder, mapping[sequence] + numbers))
 
 
 ############################################
@@ -132,6 +112,8 @@ train.sort()
 validate = list(set(samples) - set(train) - set(duplicates))
 validate.sort()
 
+# Remove T2_Sag and T2_Cor if they are part of the image sequences, as we only want image in the same orientation as the segmentations in the Train/Validate/Test setup.
+updated_image_sequences = [seq for seq in image_sequences if seq not in ['PatientID','T2_Sag', 'T2_Cor']]
 
 for sample in samples:
 
@@ -149,18 +131,15 @@ for sample in samples:
     numbers = ''.join([n for n in sample if n.isdigit()])
 
     # Copy the images
-    for img in ['tra-', 'adc-', 'hbv-']:
-        copy_image(os.path.join(output_path, 'Images', sample, img + numbers),
-                   os.path.join(dst, img + numbers))
+    for sequence in updated_image_sequences:
+        copy_image(os.path.join(output_path, 'Images', sample, mapping[sequence] + numbers), os.path.join(dst, mapping[sequence] + numbers))
 
     # Copy segmentations
     if ('Train' in dst) or ('Validate' in dst):
         # Singles from training & validating.
-        copy_segmentation(os.path.join(path_to_segmentations, 'Singles', 'Seg-' + numbers + '.nrrd'),
-                   os.path.join(dst, 'Seg-' + numbers + '.nrrd'))
+        copy_segmentation(os.path.join(path_to_segmentations, 'Singles', 'Seg-' + numbers + '.nrrd'), os.path.join(dst, 'Seg-' + numbers + '.nrrd'))
 
     elif 'Test' in dst:
         # Duplicates for testing.
         for r in ['R1', 'R2']:
-            copy_segmentation(os.path.join(path_to_segmentations, 'Duplicates', r, 'Seg-' + numbers + '_' + r + '.nrrd'),
-                       os.path.join(dst, 'Seg-' + numbers + '_' + r + '.nrrd'))
+            copy_segmentation(os.path.join(path_to_segmentations, 'Duplicates', r, 'Seg-' + numbers + '_' + r + '.nrrd'), os.path.join(dst, 'Seg-' + numbers + '_' + r + '.nrrd'))
